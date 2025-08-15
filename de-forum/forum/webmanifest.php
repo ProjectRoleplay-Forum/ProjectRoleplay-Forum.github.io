ccff4ede78d0660612ae044728713b4065160aff
ProjectRoleplay-Forum <Luka.bettinger@gmail.com>
2025-08-15 17:19:55 +0200

    Add files via upload

---
+++ de-forum/forum/webmanifest.php
@@ -0,0 +1,13 @@
+{
+    "name": "Unreal Roleplay | Forum",
+    "short_name": "Unreal Roleplay",
+    "description": "Unreal Roleplay | Forum",
+    "lang": "en-US",
+    "dir": "ltr",
+    "display": "standalone",
+    "scope": "\/forum\/",
+    "id": "\/forum\/?_pwa=1",
+    "start_url": "\/forum\/?_pwa=1",
+    "background_color": "#171e29",
+    "theme_color": "#fed500"
+}
